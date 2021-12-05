de78033a7090f444f8937cbbdbc157fe882686cc
Simon Scholz <44445356+simon-scholz@users.noreply.github.com>
2021-12-05 21:20:02 +0100

    folder re-structuring

---
+++ components/icons/index.js
@@ -0,0 +1,19 @@
+import Check from "./Check"
+import Close from "./Close"
+import Collection from "./Collection"
+import Exclamation from "./Exclamation"
+import Menu from "./Menu"
+import Moon from "./Moon"
+import Send from "./Send"
+import Sun from "./Sun"
+
+export {
+    Check,
+    Close,
+    Collection,
+    Exclamation,
+    Menu,
+    Moon,
+    Send,
+    Sun
+}
