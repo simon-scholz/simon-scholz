da6f50d189740c7aecf20ba8cb3c49db48d3ef67
Simon Scholz <44445356+simon-scholz@users.noreply.github.com>
2022-03-31 18:14:46 +0200

    Added splitbee analytics

--- pages/index.js
+++ pages/index.js
@@ -32,6 +32,7 @@ export default function Home() {
       <meta name="keywords" content="UX, UI, Product Design, Development" />
       <meta name="author" content="Simon Scholz" />
       <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no" />
+      <script async src="https://cdn.splitbee.io/sb.js"></script>
 
 
 
