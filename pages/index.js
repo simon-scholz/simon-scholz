5561e9a0735eba6dd8572a91f79e7a70469bf7ca
Simon Scholz <44445356+simon-scholz@users.noreply.github.com>
2022-03-31 18:17:29 +0200

    Revert "Added splitbee analytics"
    This reverts commit da6f50d189740c7aecf20ba8cb3c49db48d3ef67.

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,6 @@ export default function Home() {
       <meta name="keywords" content="UX, UI, Product Design, Development" />
       <meta name="author" content="Simon Scholz" />
       <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no" />
-      <script async src="https://cdn.splitbee.io/sb.js"></script>
 
 
 
